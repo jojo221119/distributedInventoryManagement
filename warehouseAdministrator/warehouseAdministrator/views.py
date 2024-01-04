1325d75e1069dea357520cdcb13af6b9a815093b
Luca Schwarz <lucky.f-black@web.de>
2024-01-04 11:12:15 +0100

    remove django app

--- warehouseAdministrator/warehouseAdministrator/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import MeinModell
-
-def startseite(request):
-    eintraege = MeinModell.objects.all()
-    return render(request, 'startseite.html', {'eintraege': eintraege})
